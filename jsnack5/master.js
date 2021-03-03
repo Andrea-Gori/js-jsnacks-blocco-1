64db85da6cd1b10863bfe2af664c050611d5f4aa
Andrea Gori <Andrea.fr.gori@gmail.com>
2021-03-03 17:26:12 +0100

    esercizio 5 completo

--- jsnack5/master.js
+++ jsnack5/master.js
@@ -1,6 +1,12 @@
 var contenitore = []
 
 for (var i = 0; i < 6; i++) {
-  var numeri = prompt('dammi un numero')
-  console.log(numeri);
+  var numero = parseInt(prompt('dammi un numero'));
+  console.log(numero);
+
+  if (numero % 2 == 1) {
+    contenitore.push(numero)
+  }
 }
+
+console.log(contenitore);
